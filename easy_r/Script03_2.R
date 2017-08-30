5f837f354d4ea2b6e4d35a2c791e227f8ff92945
aker <mageaker@hotmail.com>
2017-08-30 23:48:12 +0900

    170830 DoitR first commit

---
+++ easy_r/Script03_2.R
@@ -0,0 +1,13 @@
+x<-c(1, 2, 3)
+x
+mean(x)
+max(x)
+min(x)
+str5<-c("Hello!", "World", "is", "good!")
+str5
+paste(str5, collapse=",")
+paste(str5, collapse=" ")
+x_mean <- mean(x)
+x_mean
+str5_paste<-paste(str5, collapse=" ")
+str5_paste
---
+++ easy_r/script03_exercise.R
@@ -0,0 +1,4 @@
+scores<-c(80, 60, 70, 50, 90)
+scores
+scores_mean <-mean(scores)
+scores_mean
