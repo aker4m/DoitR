95a52a036a9fe6c0336fa3023bf1119b3edc538c
aker <mageaker@hotmail.com>
2017-09-05 21:52:49 +0900

    170905 summaries(), group_by() second

--- easy_r/script06_6.R
+++ easy_r/script06_6.R
@@ -13,3 +13,28 @@ mpg %>%
   group_by(manufacturer, drv) %>%
   summarise(mean_cty = mean(cty)) %>%
   head(10)
+mpg %>%
+  group_by(manufacturer) %>%
+  filter(class == "suv") %>%
+  mutate(tot = (cty+hwy)/2) %>%
+  summarise(mean_tot = mean(tot)) %>%
+  arrange(desc(mean_tot)) %>%
+  head(5)
+
+#Exercise
+mpg %>%
+  group_by(class) %>%
+  summarise(mean_cty = mean(cty)) %>%
+  arrange(desc(mean_cty)) 
+
+mpg %>%
+  group_by(class) %>%
+  summarise(mean_hwy = mean(hwy)) %>%
+  arrange(desc(mean_hwy)) %>%
+  head(3)
+
+mpg %>%
+  filter(class == "compact") %>%
+  group_by(manufacturer) %>%
+  summarise(n=n()) %>%
+  arrange(desc(n))
