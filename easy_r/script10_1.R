b8483ec301b0b092405ca77535b65c6e3be6b38c
aker <mageaker@hotmail.com>
2017-09-18 15:59:48 +0900

    170918 wordcloud twitter

--- easy_r/script10_1.R
+++ easy_r/script10_1.R
@@ -79,9 +79,35 @@ top20
 
 library(ggplot2)
 order <- arrange(top20, freq)$word
+theme_set(theme_bw(base_family = "AppleGothic"))
 ggplot(data = top20, aes(x = word, y = freq))+
   ylim(0, 2500)+
   geom_col()+
   coord_flip()+
   scale_x_discrete(limit = order) +
   geom_text(aes(label = freq), hjust = -0.3)
+
+pal <- brewer.pal(8, "Dark2")
+set.seed(1234)
+wordcloud(words = df_word$word,
+          freq = df_word$freq,
+          min.freq = 10,
+          max.words = 200,
+          random.order = F,
+          rot.per = .1,
+          scale = c(6, 0.2),
+          color = pal,
+          family="AppleGothic")
+
+pal <- brewer.pal(9, "Blues")[5:9]
+set.seed(1234)
+
+wordcloud(words = df_word$word,
+          freq = df_word$freq,
+          min.freq = 10,
+          max.words = 200,
+          random.order = F,
+          rot.per = .1,
+          scale = c(6, 0.2),
+          colors = pal,
+          family="AppleGothic")
